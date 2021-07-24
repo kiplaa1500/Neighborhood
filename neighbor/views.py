e58db475328308e6b0a9bc001826418ac1929942
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-24 21:12:10 +0300

    View function to render method for profile, index, user profile

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+    return render(request,'index.html')
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
+    return render(request,'profile/user_profile.html',{"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
