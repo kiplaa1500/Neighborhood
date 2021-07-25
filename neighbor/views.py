b529fbdc10828e9e6cd5e13da621f9652c95c291
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-07-25 20:07:35 +0300

    View function to render blog and blog-view

--- neighbor/views.py
+++ neighbor/views.py
@@ -59,4 +59,33 @@ def update_profile(request):
 
     return render(request,'profile/update_profile.html',{"form":form})
 
+@login_required(login_url='/accounts/login/')
+def blog(request):
+    current_user=request.user
+    profile=Profile.objects.get(username=current_user)
+    blogposts = BlogPost.objects.filter(neighbourhood=profile.neighbourhood)
+
+    return render(request,'blogs.html',{"blogposts":blogposts})
+
+@login_required(login_url='/accounts/login/')
+def view_blog(request,id):
+    current_user = request.user
+
+    try:
+        comments = Comment.objects.filter(post_id=id)
+    except:
+        comments =[]
+
+    blog = BlogPost.objects.get(id=id)
+    if request.method =='POST':
+        form = CommentForm(request.POST,request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.username = current_user
+            comment.post = blog
+            comment.save()
+    else:
+        form = CommentForm()
+
+    return render(request,'read.html',{"blog":blog,"form":form,"comments":comments})
 
